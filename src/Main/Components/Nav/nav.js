86584b026275a81ac610e55f70952c8878334f50
Devin Blankenship <failure2fly@gmail.com>
2021-07-06 13:44:17 -0700

    Made navbrand a <Link> for Router

--- src/Main/Components/Nav/nav.js
+++ src/Main/Components/Nav/nav.js
@@ -71,7 +71,7 @@ export default function Nav({setSignIn, loggedIn, setLoggedIn, name, signIn}){
         <div className="Nav sticky-top">
             <nav className="navbar navbar-expand-lg ">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="/">Reel Collaboration</a>
+                    <Link className="navbar-brand" to="/">Reel Collaboration</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                     </button>
